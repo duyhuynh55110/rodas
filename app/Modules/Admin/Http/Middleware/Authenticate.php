d496d438a78f9a55d67be3ee9e1e996b6fb0da56
duyhuynh <duyhuynh55110@gmail.com>
2022-07-10 11:22:15 +0700

    add admin guard, .env.dev

--- app/Modules/Admin/Http/Middleware/Authenticate.php
+++ app/Modules/Admin/Http/Middleware/Authenticate.php
@@ -17,7 +17,7 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if(!$request->expectJson()) {
+        if(!$request->expectsJson()) {
             return routeAdmin('login');
         }
     }
