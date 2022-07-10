80116997705a9d69a8e8dc227f5ec2d61b9e43cc
duyhuynh <duyhuynh55110@gmail.com>
2022-07-10 10:54:52 +0700

    add constants and helper

---
+++ app/Libs/Constants/constants.php
@@ -0,0 +1,4 @@
+<?php
+defined('ADMIN_MODULE_AS') or define('ADMIN_MODULE_AS', 'admin.');
+defined('ADMIN_GUARD') or define('ADMIN_GUARD', 'admin');
+defined('ADMIN_ASSET_PATH') or define('ADMIN_ASSET_PATH', 'admin-lte/');
