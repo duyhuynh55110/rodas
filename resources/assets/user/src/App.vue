3857b4c8dea53d889e586cfabf46ce2f455ea07e
duyhuynh <duyhuynh55110@gmail.com>
2022-09-06 05:41:55 +0700

    upload source end-user

---
+++ resources/assets/user/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <p> {{ this.count }} </p>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+        count: 1225,
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
