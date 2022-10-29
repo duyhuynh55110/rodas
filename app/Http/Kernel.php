29692ac4ff0799ea8745520fdd29db5c1a32c470
duyhuynh <duyhuynh55110@gmail.com>
2022-10-29 09:22:29 +0700

    install Api module

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'response.json' => \App\Modules\Api\Http\Middleware\ResponseWithJsonMiddleware::class,
     ];
 }
--- app/Modules/Admin/Routes/routes.php
+++ app/Modules/Admin/Routes/routes.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(
     [
-        // 'domain' => env('APP_ADMIN_DOMAIN'),
+        'domain' => env('APP_ADMIN_DOMAIN'),
         'as' => ADMIN_MODULE_AS,
         'middleware' => ['web'],
     ],
