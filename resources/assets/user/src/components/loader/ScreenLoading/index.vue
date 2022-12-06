012451b403f1b896fd3511bc46a5c5ba16909be5
Duy <usercongty@LFTV-LT-063.local>
2022-12-06 22:00:27 +0700

    fix screen loading css

--- resources/assets/user/src/components/loader/ScreenLoading/index.vue
+++ resources/assets/user/src/components/loader/ScreenLoading/index.vue
@@ -1,14 +1,19 @@
 <template>
     <div class="screen-loading">
         <div class="container">
-            <div class="logo">
-                <img class="logo-icon" :src="require('@/assets/img/logo.png')" />
-            </div>
+            <div class="loading-icon">
+                <div class="logo">
+                    <img
+                        class="logo-icon"
+                        :src="require('@/assets/img/logo.png')"
+                    />
+                </div>
 
-            <img
-                class="three-dots-icon"
-                :src="require('@/assets/img/svg/screen-loading.svg')"
-            />
+                <img
+                    class="three-dots-icon"
+                    :src="require('@/assets/img/svg/screen-loading.svg')"
+                />
+            </div>
         </div>
     </div>
 </template>
