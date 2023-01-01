ca0e35e4a9d9ff74a08f86bdd676a5aeeecf3201
duyhuynh <duyhuynh55110@gmail.com>
2023-01-01 11:18:08 +0700

    complete setup call api and show data

--- resources/assets/user/src/views/auth/products/ProductDetailView/ProductDetailView.js
+++ resources/assets/user/src/views/auth/products/ProductDetailView/ProductDetailView.js
@@ -1,11 +1,35 @@
 // components
 import { Stepper } from "@/components"
+import { productService } from "@/services"
+import { useRoute } from "vue-router";
 import TabSwiper from "./components/TabSwiper/TabSwiper.vue"
+
+// fetch product by id
+const fetchProductById = async function (id) {
+    const response = await productService.getProductById(id);
+
+    return response;
+}
 
 export default {
     name: "ProductDetailView",
     components: {
         Stepper,
         TabSwiper
+    },
+    setup: async function () {
+        const route = useRoute();
+        const id = route.params.id; // product id
+
+        const { data:product } = await fetchProductById(id);
+
+        return {
+            product,
+        }
+    },
+    computed: {
+        brand: function () {
+            return this.product.brand;
+        }
     }
 }
--- resources/assets/user/src/views/auth/products/ProductDetailView/components/TabSwiper/TabSwiper.vue
+++ resources/assets/user/src/views/auth/products/ProductDetailView/components/TabSwiper/TabSwiper.vue
@@ -12,18 +12,7 @@
 
         <!-- Tab Swiper -->
         <swiper @swiper="onSwiper" @init="onSlideChange" @slideChange="onSlideChange">
-            <swiper-slide>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </p>
-                <p>
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam
-                </p>
-            </swiper-slide>
+            <swiper-slide> {{ product.description }} </swiper-slide>
             <swiper-slide>Slide 2</swiper-slide>
             <swiper-slide>Slide 3</swiper-slide>
         </swiper>
