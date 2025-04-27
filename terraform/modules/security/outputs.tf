1d07a6bd664b69518629aefda05981f09009183b
duyhuynh <duyhuynh55110@gmail.com>
2025-04-27 21:55:42 +0700

    feat: remove unused modules

--- terraform/modules/security/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "alb_security_group_id" {
-  description = "The ID of the ALB security group"
-  value       = aws_security_group.alb.id
-}
--- terraform/modules/security/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "environment" {
-  description = "Environment name (e.g., dev, test)"
-  type        = string
-}
-
-variable "vpc_id" {
-  description = "VPC ID"
-  type        = string
-}
