8a9e53af877a3ccbb749ed854f5764863553eea5
duyhuynh <duyhuynh55110@gmail.com>
2025-06-26 22:04:38 +0700

    feat: update Logs system

---
+++ app/Http/Middleware/RequestLogging.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+/**
+ * RequestLogging
+ */
+class RequestLogging
+{
+    /**
+     * Hide sensitive data when logging
+     *
+     * @var array
+     */
+    const SENSITIVE_FIELDS = ['password', '_token', 'authorization', 'api_key', 'secret'];
+
+    /**
+     * Hide sensitive data by replacing values with asterisks
+     *
+     * @param array $data
+     * @return array
+     */
+    private function hideSensitiveData($data)
+    {
+        foreach (self::SENSITIVE_FIELDS as $field) {
+            if (isset($data[$field])) {
+                $data[$field] = '[SENSITIVE]';
+            }
+        }
+
+        return $data;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        // Logs for the Request
+        $this->requestLogging($request);
+
+        // Proceed with the Request
+        $response = $next($request);
+
+        // Logs for the Response
+        $this->responseLogging($response);
+
+        return $response;
+    }
+
+    /**
+     * Log incoming request details with sensitive data filtered
+     *
+     * @return void
+     */
+    private function requestLogging(Request $request)
+    {
+        $context = [
+            'url' => $request->fullUrl(),
+            'method' => $request->method(),
+            'parameters' => $this->hideSensitiveData($request->all()),
+        ];
+
+        // Only log headers for production and local environment to save cost
+        if (in_array(env('APP_ENV'), ['prod', 'local'])) {
+            $context['headers'] = $this->hideSensitiveData($request->header());
+        }
+
+        Log::info('[request]', $context);
+    }
+
+    /**
+     * Log response details with JSON content validation
+     *
+     * @param  Response  $response
+     * @return void
+     */
+    private function responseLogging($response)
+    {
+        json_decode($response->getContent(), true);
+        $context = [
+            'status' => $response->status(),
+            'content' => json_last_error() == JSON_ERROR_NONE ? $response->getContent() : '',
+        ];
+
+        Log::info('[response]', $context);
+    }
+}
---
+++ app/Libs/Loggers/LogFormatter.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Libs\Loggers;
+
+use Monolog\Formatter\LineFormatter;
+
+/**
+ * LogFormatter class
+ */
+class LogFormatter
+{
+    /**
+     * Length of request ID
+     *
+     * @var int
+     */
+    const LOGGING_UID_LENGTH = 10;
+
+    /**
+     * Format of log line
+     *
+     * @var string
+     */
+    const LOGGING_LINE_FORMAT = "[%datetime%][%uuid%] %channel%.%level_name%: %message% %context% %extra%\n";
+
+    /**
+     * Uid
+     *
+     * @var string
+     */
+    private $uid;
+
+    /**
+     * __construct
+     */
+    public function __construct(\Illuminate\Http\Request $request)
+    {
+        $this->uid = $this->generateUid(self::LOGGING_UID_LENGTH);
+    }
+
+    /**
+     * Customize the given logger instance.
+     *
+     * @param  \Illuminate\Log\Logger  $logger
+     * @return void
+     */
+    public function __invoke($logger)
+    {
+        collect($logger->getHandlers())->each(
+            function ($handler) {
+                $handler->setFormatter($this->getLogFormatter());
+            }
+        );
+    }
+
+    /**
+     * Log formatter
+     *
+     * @return mixed
+     */
+    protected function getLogFormatter()
+    {
+        $format = str_replace('%uuid%', $this->uid, self::LOGGING_LINE_FORMAT);
+
+        return new LineFormatter($format, null, true, true);
+    }
+
+    /**
+     * Get uid
+     *
+     * @return string
+     */
+    public function getUid(): string
+    {
+        return $this->uid;
+    }
+
+    /**
+     * Reset uid
+     *
+     * @return void
+     */
+    public function reset()
+    {
+        $this->uid = $this->generateUid(strlen($this->uid));
+    }
+
+    /**
+     * Generate uid
+     *
+     * @param  int  $length
+     * @return string
+     */
+    private function generateUid(int $length): string
+    {
+        return substr(bin2hex(random_bytes((int) ceil($length / 2))), 0, $length);
+    }
+}
