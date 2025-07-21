e5b3923e2b9f4cd0e404a2871665c7a55a9ccdab
duyhuynh <duyhuynh55110@gmail.com>
2025-07-21 21:59:16 +0700

    feat: change structure

--- terraform/modules/log_group/output.tf
+++
@@ -1,9 +0,0 @@
-output "log_group_arn" {
-  description = "The ARN of the CloudWatch log group."
-  value = aws_cloudwatch_log_group.log_group.arn
-}
-
-output "log_group_name" {
-  description = "The name of the CloudWatch log group."
-  value = aws_cloudwatch_log_group.log_group.name
-}
