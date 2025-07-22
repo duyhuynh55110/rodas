150898e6fceac0395a80ff379525c3bbeb74479c
DuyHT <duyht21@lifull-tech.vn>
2025-07-22 10:35:42 +0700

    feat: add record to route53

--- terraform/modules/route53/variables.tf
+++ terraform/modules/route53/variables.tf
@@ -13,6 +13,16 @@ variable "alb_zone_id" {
   type        = string
 }
 
+variable "cf_distribution_domain_name" {
+  description = "The domain name of the CloudFront distribution (e.g., d1234567890abcdef.cloudfront.net)."
+  type        = string
+}
+
+variable "cf_distribution_hosted_zone_id" {
+  description = "The hosted zone ID of the CloudFront distribution (e.g., Z2FDTNDATAQYW2)."
+  type        = string
+}
+
 variable "common_tags" {
   description = "Common tags to apply to all resources"
   type        = map(any)
